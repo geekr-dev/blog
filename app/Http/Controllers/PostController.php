3ce5fcbc3043fc4b9ab064189b3e1548419aa395
geekr <geekr@hey.com>
2022-12-06 21:51:13 +0800

    实现博客文章增删改查+页面渲染

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use Inertia\Inertia;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return Inertia::render('Posts/Index', ['posts' => $posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Posts/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'slug' => 'required|string|max:255',
+            'content' => 'required',
+        ]);
+
+        $post = new Post([
+            'title' => $request->title,
+            'slug' => Str::slug($request->slug),
+            'content' => $request->content,
+        ]);
+        $post->user_id = $request->user()->id;
+        $post->save();
+
+        return redirect()->route('posts.index')->with('message', 'Post Created Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        return Inertia::render('Posts/Edit', ['post' => $post]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'slug' => 'required|string|max:255',
+            'content' => 'required',
+        ]);
+
+        $post->title = $request->title;
+        $post->slug = Str::slug($request->slug);
+        $post->content = $request->content;
+        $post->user_id = $request->user()->id;
+        $post->save();
+
+        return redirect()->route('posts.index')->with('message', 'Post Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('posts.index')->with('message', 'Post Delete Successfully');
+    }
+}
---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,111 @@
+<script setup>
+import BreezeAuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/inertia-vue3";
+import BreezeButton from "@/Components/Button.vue";
+import { Link } from "@inertiajs/inertia-vue3";
+import { Inertia } from "@inertiajs/inertia";
+import { useForm } from '@inertiajs/inertia-vue3'
+
+const props = defineProps({
+    posts: {
+        type: Object,
+        default: () => ({}),
+    },
+});
+const form = useForm();
+
+function destroy(id) {
+    if (confirm("Are you sure you want to Delete")) {
+        form.delete(route('posts.destroy', id));
+    }
+}
+</script>
+
+<template>
+
+    <Head title="Posts" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Posts Index
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div v-if="$page.props.flash.message"
+                    class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg dark:bg-green-200 dark:text-green-800"
+                    role="alert">
+                    <span class="font-medium">
+                        {{ $page.props.flash.message }}
+                    </span>
+                </div>
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <div class="mb-2">
+                            <Link :href="route('posts.create')">
+                            <BreezeButton>New Post</BreezeButton>
+                            </Link>
+                        </div>
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                <thead
+                                    class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">#</th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Title
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Slug
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Edit
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Delete
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="post in posts" :key="post.id"
+                                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                            {{ post.id }}
+                                        </th>
+                                        <th scope="row"
+                                            class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                            {{ post.title }}
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            {{ post.slug }}
+                                        </td>
+
+
+                                        <td class="px-6 py-4">
+                                            <Link :href="
+                                                route(
+                                                    'posts.edit',
+                                                    post.id
+                                                )
+                                            " class="px-4 py-2 text-white bg-blue-600 rounded-lg">Edit</Link>
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <BreezeButton class="bg-red-700" @click="destroy(post.id)">
+                                                Delete
+                                            </BreezeButton>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </BreezeAuthenticatedLayout>
+</template>
